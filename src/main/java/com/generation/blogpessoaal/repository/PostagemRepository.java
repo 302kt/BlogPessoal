da4cc6838d97e4872a0d4ff0ea1891a15fb556fd
katarina pereira <kpf.freire@gmail.com>
2023-04-13 14:17:30 -0300

    Add @Repository

--- src/main/java/com/generation/blogpessoaal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoaal/repository/PostagemRepository.java
@@ -1,9 +1,11 @@
 package com.generation.blogpessoaal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoaal.model.Postagem;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 }
